5b64cf19357956a0e41253f95c13cc9d20c7c619
System Administrator <root@Parths-MacBook-Air.local>
2024-02-08 18:58:45 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { Router, Route, navigate } from "svelte-navigator";
+  import "font-awesome/css/font-awesome.css";
+  import LoginPage from "./pages/Auth/login-page/LoginPage.svelte";
+  import RegisterPage from "./pages/Auth/register-page/RegisterPage.svelte";
+  import Authguard from "./routing/Authguard.svelte";
+  import Navigate from "./routing/Navigate.svelte";
+  
+  import UpdatePassword from "./pages/Auth/update-password/UpdatePassword.svelte";
+  import ResetPassword from "./pages/Auth/reset-password/ResetPassword.svelte";
+  import ForgotPassword from "./pages/Auth/forgot-password/ForgotPassword.svelte";
+  
+  import { onMount } from "svelte";
+
+
+  export let url = "/";
+  
+
+  onMount(async () => {
+    let isloggedIn;
+   
+  });
+</script>
+
+<Router {url}>
+  <Authguard>
+    <!-- <section slot="loggedIn">
+      <Route path="/dashboard/*" component={Dashboard} />
+      <Route path="/*"><Navigate to="/dashboard" /></Route>
+    </section> -->
+    <section slot="unauthorized">
+      <Route path="/forgot/password" component={ForgotPassword} />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/register" component={RegisterPage} />
+      <Route path="/update/password" component={UpdatePassword} />
+      <Route path="/reset/password" component={ResetPassword} />
+      <Route path="/*"><Navigate to="/login" /></Route>
+    </section>
+  </Authguard>
+</Router>
+
+<Toast />
