bf9efcc21430037f786cdd2986b2dd59a61ad9a2
Md Asif Raza <itsmdasifraza@gmail.com>
2024-12-19 16:57:08 +0530

    feat: Merge branch 'feat/web-auth-copy-link' of github.com:itsmdasifraza/sparrow-auth into feat/web-auth-copy-link

--- src/pages/Auth/oauth-redirect/OauthRedirect.svelte
+++ src/pages/Auth/oauth-redirect/OauthRedirect.svelte
@@ -4,13 +4,7 @@
 	import Redirect from '../redirect/Redirect.svelte';
 	import constants from '$lib/utils/constants';
 	import RegisterRedirectComponent from '../redirect/RegisterRedirectComponent.svelte';
-
 	import { notifications } from '$lib/components/toast-notification/ToastNotification';
-
-
-	import { notifications } from '$lib/components/toast-notification/ToastNotification';
-	import { jwtDecode } from '$lib/utils/jwt';
-
 
 	let userFromDesktop = localStorage.getItem('isUserFromDesktop');
 
