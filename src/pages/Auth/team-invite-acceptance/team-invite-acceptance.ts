28938a5f7abb8efe4057f478ca4148f8a8b6601a
disha <dishachhabra173@gmail.com>
2025-04-16 19:43:53 +0530

    redirect page changes

--- src/pages/Auth/team-invite-acceptance/team-invite-acceptance.ts
+++ src/pages/Auth/team-invite-acceptance/team-invite-acceptance.ts
@@ -1,12 +1,15 @@
 import { notifications } from "$lib/components/toast-notification/ToastNotification";
 import { acceptTeamInvite } from "$lib/services/auth.service";
+import { navigate } from "svelte-navigator";
 export const handleAcceptTeamInvite = async (
   teamId: string, inviteId: string, email: string
 ) => {
-  const response = await acceptTeamInvite(teamId, inviteId);
+  const response = await acceptTeamInvite(teamId, email);
   if (response.isSuccessful) {
-		notifications.success(response?.message || "Invitation accepted successfully");
-	} else{
-    notifications.error(response.message || "Failed to accept invitation");
+    // Redirect to the sparrow app
+    //  window.location.href = 'sparrow://';
+  } else {
+    //  window.location.href = 'sparrow://';
+    notifications.error(response?.message || "Failed to accept invitation");
   }
 };
